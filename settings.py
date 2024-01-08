af24a8b47a529b0483f1c2a52f107cde8d7bbd75
Joker pp_e <voix.momotalk@gmail.com>
2024-01-08 11:58:02 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 from typing import Final
 
 
-TOKEN: Final = '6301865249:AAFgRnv5Ev9QAyBuRl8Gq4o51aNK268qc_E'
-BOT_USER_NAME: Final = '@assistant_ai_x_super_sp_d65_bot'
+TOKEN: Final = ''
+BOT_USER_NAME: Final = ''
 SUPER_USER_NAME: Final = [
-    'sp.d65'
+    ''
 ]
 GROUP_CHAT_ID: Final = -1002109063811
