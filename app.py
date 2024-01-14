b911839392c754730dec98830854ded4cfd25cbf
Joker pp_e <voix.momotalk@gmail.com>
2024-01-14 21:37:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text: str = update.message.text
 
     # print('User @'+update.effective_user.username +'['+update.effective_user.first_name+' ' + update.effective_user.last_name+'] in ' + message_type + ": " + text)
-    print('Input: ' + text)
+    print(f'Input: {text} from ${str(update.effective_chat.title)}')
 
     group_white_list = [
        GROUP_CHAT_ID
