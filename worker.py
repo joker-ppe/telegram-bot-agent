03031688f81807eb2c69b55846f419d526e26e82
Joker pp_e <voix.momotalk@gmail.com>
2024-04-07 15:34:52 +0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -7,8 +7,10 @@ from settings import MASTER_USER_NAME, SUPER_USER_NAME
 
 from style import get_style
 
+# baseUrl = "http://3.1.5.108"
+baseUrl = "http://13.212.127.193"
 
-baseUrl = "http://3.1.5.108"
+
 # baseUrl = "https://api.winwwin68.com"
 # baseUrl = "http://localhost:3004"
 
@@ -20,6 +22,7 @@ async def fetch_url(session, url):
             return await response.json()
         else:
             return ''
+
 
 async def get_user(from_date, end_date, yesterday, user_name):
     url = f'{baseUrl}/super/user?'
@@ -33,7 +36,8 @@ async def get_user(from_date, end_date, yesterday, user_name):
         if (len(response) == 0):
             return '***'
         return response
-    
+
+
 # async def get_user_data(from_date, end_date, user_name, filter='profit'):
 #     url = f'{baseUrl}/report?startDate={from_date}&endDate={end_date}&userName={user_name}'
 #     async with aiohttp.ClientSession() as session:
@@ -41,8 +45,8 @@ async def get_user(from_date, end_date, yesterday, user_name):
 #         if (len(response) == 0):
 #             return '***'
 #         return response[filter]
-        # print(response)
-    
+# print(response)
+
 # async def get_report_number(end_date):
 #     url = f'{baseUrl}/report/numbers?endDate={end_date}'
 #     async with aiohttp.ClientSession() as session:
@@ -63,6 +67,7 @@ async def get_user_os_bet(end_date, user_name):
         if (len(response) == 0):
             return '***'
         return response
+
 
 # async def get_user_profit(from_date, end_date, user_name):
 #     response = await get_user_data(from_date, end_date, user_name, filter='profit')
@@ -89,6 +94,7 @@ async def get_report_xsmb():
     html_table += "</body></html>"
     return html_table
 
+
 async def get_supers(from_date, end_date):
     url = f'{baseUrl}/super/supers?'
     for super_user_name in SUPER_USER_NAME:
@@ -102,7 +108,8 @@ async def get_supers(from_date, end_date):
         if (len(response) == 0):
             return '***'
         return response
-    
+
+
 async def get_masters(from_date, end_date):
     url = f'{baseUrl}/super/masters?'
     for super_user_name in SUPER_USER_NAME:
@@ -116,7 +123,8 @@ async def get_masters(from_date, end_date):
         if (len(response) == 0):
             return '***'
         return response
-    
+
+
 async def get_agents(from_date, end_date):
     url = f'{baseUrl}/super/agents?'
     for super_user_name in SUPER_USER_NAME:
@@ -131,6 +139,7 @@ async def get_agents(from_date, end_date):
             return '***'
         return response
 
+
 async def get_members(from_date, end_date):
     url = f'{baseUrl}/super/members?'
     for super_user_name in SUPER_USER_NAME:
@@ -144,7 +153,8 @@ async def get_members(from_date, end_date):
         if (len(response) == 0):
             return '***'
         return response
-    
+
+
 async def get_members_inactive(from_date, end_date):
     url = f'{baseUrl}/super/membersInactive?'
     for super_user_name in SUPER_USER_NAME:
